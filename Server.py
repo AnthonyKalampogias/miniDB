96284f3bc64884ddca833f63ce0ad5bbc8dfba07
Anthony_Kalampogias <44137095+AnthonyKalampogias@users.noreply.github.com>
2021-01-09 17:56:14 +0200

    Feat: Server can now execute more than one queries from each client

--- Server.py
+++ Server.py
@@ -10,45 +10,56 @@ PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
 # Function to run when a clinet connects to the server
 
-def clientConnect():
+def clientConnect(receivedData, data):
+    #receivedData is a flag that tells the function to either wait for input(first run from client) line 21
+    # or either tell it that I have a new input from the client so just execute that
+    # data is the variable that I store the desired query and it contains a value only when the function is recursivly called
+
     sendToClient = False  # if true the server will send the response to the client
     print('Connected by', addr)
 
-    while True:
-
+    if receivedData:  # If this is the first query, wait for input
         data = conn.recv(1024)  # Receive query
         data = data.decode("utf-8")  # Decode it
-        
-        data = data.lower() # Sanitize user input to check what he wants to execute
-        if "database" in data: # Check if the user wants to create a database
-            data = data.split("'")
-            db = Database(data[1], load=False)
+        data = data.lower()  # Sanitize user input to check what he wants to execute
+
+    if "database" in data:  # Check if the user wants to create a database
+        data = data.split("'")
+        db = Database(data[1], load=False)
+    else:
+        # create db with name "clientCB"
+        db = Database('clientCB', load=False)
+        FinalQuery = "db.{}".format(data)
+        # Add to a variable with the db object along with the received query from the client
+        print("Client {} requested to execute the query {}".format(addr, FinalQuery))
+
+        old_stdout = sys.stdout
+        result = StringIO()
+        sys.stdout = result
+        # Above 3 lines are standard to save the stdout that python uses with print()
+        # inside the variable result
+
+        if len(FinalQuery) > 3:  # Contains more than just "db." the object
+            exec(FinalQuery)  # Execute the query
+            sendToClient = True
+        if not sendToClient:
+            print("Something went wrong..\n")
+
         else:
-            # create db with name "smdb"
-            db = Database('vsmdb', load=False)
-            FinalQuery = "db.{}".format(data)
-            # Add to a variable with the db object along with the received query from the client
-            print("Client {} requested to execute the query {}".format(addr, FinalQuery))
-
-            old_stdout = sys.stdout
-            result = StringIO()
-            # Above 3 lines are standard to save the stdout that python uses with print()
-            # inside the variable result
-            sys.stdout = result
-
-            if len(FinalQuery) > 3:  # Contains more than just "db." the object
-                exec(FinalQuery)  # Execute the query
-                sendToClient = True
-            if not sendToClient:
-                print("Something went wrong..\n")
-                break
+            sys.stdout = old_stdout
+            result_string = result.getvalue()
+            print(result_string)
+            conn.sendall(str.encode(result_string))
+            data = conn.recv(1024)  # Receive query
+            data = data.decode("utf-8")  # Decode it
+            if data != "":
+                # Recursivly call it's self with setting the receivedData flag to false so it won't wait again for input
+                clientConnect(False, data)
+                    # receivedData -> False, there is already available data to execute
+                    # data -> Now has the received data from the client
             else:
-                sys.stdout = old_stdout
-                result_string = result.getvalue()
-                print(result_string)
-                conn.sendall(str.encode(result_string))
+                print("Empty Input!!!\nTerminating connection...")
                 conn.close()
-                break
 
 
 while True:
@@ -57,7 +68,10 @@ while True:
         s.listen()
         conn, addr = s.accept()
         with conn:
-            clientConnect()
-            s.close()  # Call the function that will execute the clients query and send him back the response
-            print("Clients query executed\nDropping connection..\nWaiting for next connection...\n")
+            clientConnect(True, None)  
+                # receivedData -> True, First Run
+                # data -> None, Since it is the first run no data is available
+            s.close()
+            print(
+                "Clients valid queries were executed\nDropping connection..\nWaiting for next connection...\n")
             continue  # Break out and wait for next connection
--- client.py
+++ client.py
@@ -1,17 +1,41 @@
-import socket, ast
+import socket
+import ast
 
-query= input("What is the query you would like to execute ?\n Examples: select('classroom','*')\nInput:")
+query = input(
+    "What is the query you would like to execute ?\n Examples: select('classroom','*')\nInput:")
+nextQuery = None  # At first it will be empty to trigger the question on the client if he wishes to execute more than one query but if it sets a value it won't be asked again
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    if (len(query)>6):
-        s.connect((HOST, PORT))
-        s.sendall(query.encode())
-        data = s.recv(1024)
-        print('Received response from server\n', data.decode('utf8', 'strict'))
-        s.close()
-    else:
-        print("Something wrong with your input!\n Please check the provided examples and try again :)")
+    while True:
+        if nextQuery is None: # For first run 
+            s.connect((HOST, PORT))
+            s.sendall(query.encode())
+            data = s.recv(1024)
+            print('Received response from server\n',
+                  data.decode('utf8', 'strict'))
 
+            nextQuery = input(
+                "Would you like to execute something else ?[y/n]\nIf you later wish to exit the program don't enter anything in your input and press Enter...\nInput:")
+
+        elif nextQuery == "y" or nextQuery == "Y":
+            query = input(
+                "What is the query you would like to execute ?\n Examples: select('classroom','*')\nInput:")
+            if query != "":
+                s.sendall(query.encode())
+                data = s.recv(1024)
+                print('Received response from server\n',
+                      data.decode('utf8', 'strict'))
+            else:
+                print(
+                    "Something wrong with your input!\n Please check the provided examples and try again :)")
+                s.close()
+                exit()
+
+        else:
+            print(
+                "Something wrong with your input!\n Please check the provided examples and try again :)")
+            s.close()
+            exit()
