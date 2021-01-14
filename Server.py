c525a588247dd7975beb98d7b0363e6b2ff5307b
theodore <sampleGmail@sample.com>
2021-01-14 19:24:46 +0200

    chore: removed the import pickle, not needed

--- Server.py
+++ Server.py
@@ -3,7 +3,7 @@ import socket
 import sys
 from io import StringIO
 import SQLcompiler
-import pickle
+
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
